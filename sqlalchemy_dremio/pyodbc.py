be000fd12410fa76931c84c7cf64fc0bddec7bcf
zaenal <ahmadzaenal125@gmail.com>
2022-08-01 09:42:50 +0700

    change windows and linux drivers

--- sqlalchemy_dremio/pyodbc.py
+++ sqlalchemy_dremio/pyodbc.py
@@ -11,9 +11,9 @@ class DremioExecutionContext_pyodbc(DremioExecutionContext):
 class DremioDialect_pyodbc(PyODBCConnector, DremioDialect):
     execution_ctx_cls = DremioExecutionContext_pyodbc
     driver_for_platform = {
-        'Linux 64bit': '/opt/dremio-odbc/lib64/libdrillodbc_sb64.so',
-        'Linux 32bit': '/opt/dremio-odbc/lib32/libdrillodbc_sb32.so',
-        'Windows': 'Dremio Connector',
+        'Linux 64bit': '/opt/arrow-flight-sql-odbc-driver/lib64/libarrow-odbc.so.0.9.1.168',
+        'Linux 32bit': '/opt/arrow-flight-sql-odbc-driver/lib64/libarrow-odbc.so.0.9.1.168',
+        'Windows': 'Apache Arrow Flight SQL',
         'Darwin': '/Library/Dremio/ODBC/lib/libdrillodbc_sbu.dylib'
     }
     pyodbc_driver_name = driver_for_platform[platform.system() + (' ' + platform.architecture()[0] if platform.system() == 'Linux' else '')]
